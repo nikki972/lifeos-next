50ebd2f02bc4eda7f5ce18d9d57c25a42601578a
Nikki972 <nik_vo2002@mail.ru>
2026-05-13 23:30:10 +0400

    fix turbopack config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
-});
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  turbopack: {},
+};
 
-module.exports = withPWA({});
+module.exports = nextConfig;
